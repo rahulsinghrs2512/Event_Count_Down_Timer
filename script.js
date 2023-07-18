5fc1696d4457627d7a80ff7831aa878c986eedab
rahulsinghrs2512 <96308524+rahulsinghrs2512@users.noreply.github.com>
2023-07-18 21:38:56 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+var countDownDate=new Date("oct 18,2024 18:00:00").getTime();
+var x=setInterval(()=>
+{
+    var now=new Date().getTime();
+    let distance=countDownDate-now;
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+   var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+
+    document.getElementById("days").innerHTML=days;
+    document.getElementById("hours").innerHTML=hours;
+    document.getElementById("minutes").innerHTML=minutes;
+    document.getElementById("seconds").innerHTML=seconds;
+   
+
+    if(distance<0)
+    {
+        clearInterval(x);
+        document.getElementById("days").innerHTML="00";
+        document.getElementById("hours").innerHTML="00";
+        document.getElementById("minutes").innerHTML="00";
+        document.getElementById("seconds").innerHTML="00";
+    }
+
+
+},1000)
